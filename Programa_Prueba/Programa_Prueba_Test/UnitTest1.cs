7fe398b55f2651dd61cd3b6e8995fd5f4ce8618c
AULA-210-1 <you@example.com>
2024-05-07 20:17:03 -0300

    Commit inicial

---
+++ Programa_Prueba/Programa_Prueba_Test/UnitTest1.cs
@@ -0,0 +1,16 @@
+namespace Programa_Prueba_Test
+{
+    public class UnitTest1
+    {
+        [Fact]
+       //Arrange
+        public void Test1()
+        {
+
+        }
+        //Act
+
+
+        //Assert
+    }
+}
